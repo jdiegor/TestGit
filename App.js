a5393c71804dc759e08fb40b8120ec5a94156e65
jdiegor <30551266+jdiegor@users.noreply.github.com>
2019-04-21 17:12:01 -0500

    Este es el segundo cambio

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
+    'JOSE DIEGO,\n' + 
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
